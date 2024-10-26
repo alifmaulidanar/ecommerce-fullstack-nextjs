30de972c65f6e4e24ddb70a7163cb9cf425eb0df
alifmaulidanar <alifmaulidanr@gmail.com>
2024-10-26 21:42:18 +0700

    Update Zod schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,14 +1,35 @@
 import { z } from 'zod';
 
+export const ALLOW_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+]
+
 export const schemaSignIn = z.object({
-  email: z.string({ required_error: "Email is required" }).email({ message: "Invalid email" }),
-  password: z.string({ required_error: "Password is required" }).min(5, { message: "Password must be at least 5 characters" }),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email({ message: "Invalid email" }),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(5, { message: "Password must be at least 5 characters" }),
 });
 
 export const schemaCategory = z.object({
-  name: z.string({ required_error: "Name is required" }).min(4, { message: "Name must be at least 4 characters" }),
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(4, { message: "Name must be at least 4 characters" }),
 });
 
 export const schemaLocation = z.object({
-  name: z.string({ required_error: "Name is required" }).min(4, { message: "Name must be at least 4 characters" }),
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(4, { message: "Name must be at least 4 characters" }),
+});
+
+export const schemaBrand = schemaCategory.extend({
+  image: z
+    .any()
+    .refine((file: File) => ALLOW_MIME_TYPES.includes(file.type), { message: "Invalid file type" })
+    .refine((file: File) => file?.name, { message: "Image is required" }),
 });
