55c64e8904e90e68d2fcca3ea89c7f3359af78e3
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-07 21:40:51 +0700

    Added new schema for product

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -33,3 +33,35 @@ export const schemaBrand = schemaCategory.extend({
     .refine((file: File) => ALLOW_MIME_TYPES.includes(file.type), { message: "Invalid file type" })
     .refine((file: File) => file?.name, { message: "Image is required" }),
 });
+
+export const schemaProduct = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(4, { message: "Name must be at least 4 characters" }),
+  description: z
+    .string({ required_error: "Description is required" })
+    .min(10, { message: "Description must be at least 4 characters" }),
+  price: z
+    .string({ required_error: "Price is required" }),
+  stock: z
+    .string({ required_error: "Stock is required" }),
+  brand_id: z
+    .string({ required_error: "Brand is required" }),
+  category_id: z
+    .string({ required_error: "Category is required" }),
+  location_id: z
+    .string({ required_error: "Location is required" }),
+  images: z.any()
+    .refine((files: File[]) => files.length === 3, { message: "Please upload 3 product images" })
+    .refine((files: File[]) => {
+      let validate = false
+
+      Array.from(files).find((file) => {
+        validate = ALLOW_MIME_TYPES.includes(file.type)
+      })
+
+      return validate
+    }, {
+      message: "Invalid file type"
+    })
+})
