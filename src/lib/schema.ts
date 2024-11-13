4e293319b4dcda2933ff08976f1bad04ece35868
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-13 19:29:43 +0700

    Added schema for SignUp

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -15,6 +15,12 @@ export const schemaSignIn = z.object({
     .min(5, { message: "Password must be at least 5 characters" }),
 });
 
+export const schemaSignUp = schemaSignIn.extend({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(4, { message: "Name must be at least 4 characters" }),
+})
+
 export const schemaCategory = z.object({
   name: z
     .string({ required_error: "Name is required" })
