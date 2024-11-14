f9eb14947f42be60ef776070d185ee353fb3bdf1
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-14 09:47:34 +0700

    Implementing FilterCheckboxItem in stock, brand, location, and category component

--- src/app/(customer)/(index)/catalogs/_components/filter-category.tsx
+++ src/app/(customer)/(index)/catalogs/_components/filter-category.tsx
@@ -1,5 +1,6 @@
-import { getCategories } from '@/app/(admin)/dashboard/(index)/categories/lib/data'
 import React from 'react'
+import FilterCheckboxItem from './filter-checkbox-item'
+import { getCategories } from '@/app/(admin)/dashboard/(index)/categories/lib/data'
 
 export default async function FilterCategory() {
   const categories = await getCategories()
@@ -8,15 +9,16 @@ export default async function FilterCategory() {
     <div className="flex flex-col gap-[14px]">
       <p className="font-semibold leading-[22px]">Categories</p>
       {categories.map((category) => (
-        <label key={`${category.id + category.name}`} htmlFor={`${category.id + category.name}`} className="font-semibold flex items-center gap-3">
-          <input
-            type="checkbox"
-            name="stock"
-            value={category.id}
-            className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-          />
-          <span>{category.name}</span>
-        </label>
+        // <label key={`${category.id + category.name}`} htmlFor={`${category.id + category.name}`} className="font-semibold flex items-center gap-3">
+        //   <input
+        //     type="checkbox"
+        //     name="stock"
+        //     value={category.id}
+        //     className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
+        //   />
+        //   <span>{category.name}</span>
+        // </label>
+        <FilterCheckboxItem key={category.id + category.name} id={category.id.toString()} value={category.name} type="category" />
       ))}
     </div>
   )
