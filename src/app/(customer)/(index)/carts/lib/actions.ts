97fb2733faf4c37a9412b7d34be4a4eeb40a6770
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-15 07:37:17 +0700

    Stored order detail to orderDetail and orderProduct

--- src/app/(customer)/(index)/carts/lib/actions.ts
+++ src/app/(customer)/(index)/carts/lib/actions.ts
@@ -6,12 +6,13 @@ import { ActionResult, Tcart } from "@/types";
 import { redirect } from "next/navigation";
 import prisma from "../../../../../../lib/prisma";
 import { generateRandomString } from "@/lib/utils";
+import { Prisma } from "@prisma/client";
 
 export async function storeOrder(
   _: unknown,
   formData: FormData,
   total: number,
-  product: Tcart[]
+  products: Tcart[]
 ): Promise<ActionResult> {
   const { session, user } = await getUser()
 
@@ -35,7 +36,7 @@ export async function storeOrder(
   }
 
   try {
-    await prisma.order.create({
+    const order = await prisma.order.create({
       data: {
         total: total,
         status: "pending",
@@ -43,6 +44,36 @@ export async function storeOrder(
         code: generateRandomString(15),
       }
     })
+    console.log("store to order")
+
+    const queryCreateProductOrder: Prisma.OrderProductCreateManyInput[] = []
+
+    for (const product of products) {
+      queryCreateProductOrder.push({
+        order_id: order.id,
+        product_id: product.id,
+        quantity: product.quantity,
+        subtotal: product.price
+      })
+    }
+
+    await prisma.orderProduct.createMany({
+      data: queryCreateProductOrder
+    })
+    console.log("store to orderProduct")
+
+    await prisma.orderDetail.create({
+      data: {
+        address: parse.data.address,
+        city: parse.data.city,
+        name: parse.data.name,
+        phone: parse.data.phone,
+        postalCode: parse.data.postalCode,
+        notes: parse.data.notes,
+        order_id: order.id
+      }
+    })
+    console.log("store to orderDetail")
   } catch (error) {
     console.error(error)
     return {
@@ -50,5 +81,5 @@ export async function storeOrder(
     }
   }
 
-  return redirect("/carts")
+  return redirect("/")
 }
