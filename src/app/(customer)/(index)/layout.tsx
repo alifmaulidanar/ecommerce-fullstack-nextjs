5e4ba724bc75d3f9b8ddee801c4124da44f01d2d
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-15 07:36:10 +0700

    Update metadata for customer layout

--- src/app/(customer)/(index)/layout.tsx
+++ src/app/(customer)/(index)/layout.tsx
@@ -1,10 +1,16 @@
 import "../../globalsLanding.css";
+import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   weight: ['300', '400', '500', '700', '800'],
   subsets: ['latin-ext'],
 });
+
+export const metadata: Metadata = {
+  title: "Belanja",
+  description: "Belanja E-Commerce Home",
+};
 
 export default function LandingRootLayout({
   children,
