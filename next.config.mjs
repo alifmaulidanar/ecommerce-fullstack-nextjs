0e1ea383da924f7f681afe18e9b9a9c5b34ab05d
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-22 19:09:18 +0700

    Fix CF deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -9,5 +19,9 @@ const nextConfig = {
     ],
   },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
